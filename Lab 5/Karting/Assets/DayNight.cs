be1b2780f659e80d896fe6aa931e54def11d2477
Caetano <cjhyams@ucsc.edu>
2020-05-08 19:13:26 -0700

    Lab5 push

---
+++ Lab 5/Karting/Assets/DayNight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour
+{
+    [SerializeField]
+    private float setTime;
+    private GameObject sun;
+    private bool triggered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sun = GameObject.Find("DayNight");
+    }
+
+    // Turn the sun until my set time
+    private void Update()
+    {
+        if (triggered)
+        {
+            float rotateSpeed = 10 * Time.deltaTime;
+            transform.rotation *= Quaternion.AngleAxis(rotateSpeed, Vector3.right);
+            if (sun.transform.rotation.x >= setTime)
+            {
+                triggered = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            triggered = true;
+        }
+    }
+}
