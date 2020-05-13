9d77c2427c16c2d7ae0161b3ef13a058eececdf9
Caetano <cjhyams@ucsc.edu>
2020-05-12 23:44:28 -0700

    Fixed gitignore

---
+++ Lab 6/Lab 6/Assets/SpotlightMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightMove : MonoBehaviour
+{
+    private int vel = 1;
+    private float bounce = 10;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + vel * Time.deltaTime, transform.position.y, transform.position.z);
+        bounce -= 1 * Time.deltaTime;
+        if (bounce <= 0)
+        {
+            vel *= -1;
+            bounce = 10;
+        }
+    }
+}
