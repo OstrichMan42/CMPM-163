8e244c209c3b84ac7e26662ab52523a249b3bb2a
Caetano <cjhyams@ucsc.edu>
2020-05-18 17:09:58 -0700

    Lab7 Work

---
+++ Lab7/Lab 7/Assets/SunMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMove : MonoBehaviour
+{
+    private int vel = 1;
+    private float bounce = 10;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + vel * Time.deltaTime, transform.position.z);
+        bounce -= 1 * Time.deltaTime;
+        if (bounce <= 0)
+        {
+            vel *= -1;
+            bounce = 10;
+        }
+    }
+}
